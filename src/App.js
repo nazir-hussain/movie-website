26b55c3ed9f1b1fddd0bd5f15f82f0ffb1c80b7b
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-10-17 22:32:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
